a7a1550c78616704d0f627475f0186ff6f4857a6
DimaPanchuk <dmytropanchuk0@gmail.com>
2022-06-13 00:11:18 +0300

    fix navbar

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ function burgerMenu() {
     const burger = document.querySelector('.burger')
     const menu = document.querySelector('.menu')
     const body = document.querySelector('body')
+    const links = document.querySelectorAll('.menu__item-link')
+  
     burger.addEventListener('click', () => {
       if (!menu.classList.contains('active')) {
         menu.classList.add('active')
@@ -15,36 +17,27 @@ function burgerMenu() {
         body.classList.remove('locked')
       }
     })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
+  
+  links.forEach(link => link.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  }))
+
+  window.addEventListener('resize', () => {
       if (window.innerWidth > 991.98) {
         menu.classList.remove('active')
         burger.classList.remove('active-burger')
         body.classList.remove('locked')
       }
-    })
+  })
+  
+
   }
   burgerMenu()
-  
-  
-  // Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
-  function fixedNav() {
-    const nav = document.querySelector('nav')
-  
-    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-    const breakpoint = 1
-    if (window.scrollY >= breakpoint) {
-      nav.classList.add('fixed__nav')
-    } else {
-      nav.classList.remove('fixed__nav')
-    }
-  }
-  window.addEventListener('scroll', fixedNav)
-  
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-prev',
-      prevEl: '.swiper-next',
-    }
-  });
